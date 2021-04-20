77b8f53609798ab9e4846b3f11c8fd4baca92766
anhkhoinguyen1310 <anhkhoinguyen1310@gmail.com>
2021-04-20 13:45:16 +0700

    add dropdown to public in netlify

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -17,7 +17,7 @@ export default function Navibar() {
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link< /NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Form inline>
